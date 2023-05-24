470b09d917f44def07b192d974046ca55458a2a1
Yash Garg <106302581+Eryashgarg@users.noreply.github.com>
2023-05-24 23:27:10 +0530

    Create Longest Common Prefix.java

---
+++ Day-016/Longest Common Prefix.java
@@ -0,0 +1,13 @@
+// Link to problem: https://leetcode.com/problems/longest-common-prefix/description/
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String s1 = strs[0] , s2 = strs[strs.length - 1];
+        int idx = 0;
+        while(idx<s1.length()){
+            if(s1.charAt(idx) == s2.charAt(idx)) idx++;
+            else break;
+        }
+        return s1.substring(0,idx);
+    }
+}
