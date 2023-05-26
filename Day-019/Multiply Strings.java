f7aee96e7257a57f8e55d9bbe52888bf83cc8e60
Yash Garg <106302581+Eryashgarg@users.noreply.github.com>
2023-05-26 21:03:33 +0530

    Create Multiply Strings.java

---
+++ Day-019/Multiply Strings.java
@@ -0,0 +1,10 @@
+// Link to problem: https://leetcode.com/problems/multiply-strings/description/
+import java.math.BigInteger;
+class Solution {
+    public String multiply(String num1, String num2) {
+        BigInteger n1 = new BigInteger(num1);
+        BigInteger n2 = new BigInteger(num2);
+        String str = String.valueOf(n1.multiply(n2));
+        return str;
+    }
+}
