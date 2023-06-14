c5dd05389315e1ccea7a837fff692e8db11cfe98
Yash Garg <106302581+Eryashgarg@users.noreply.github.com>
2023-06-14 19:37:17 +0530

    Create Merge Sorted Array.java

---
+++ Day-038/Merge Sorted Array.java
@@ -0,0 +1,10 @@
+// Link to problem: https://leetcode.com/problems/merge-sorted-array/submissions/971148236/
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int j = 0, i = m; j < n; j++) {
+            nums1[i] = nums2[j];
+            i++;
+        }
+        Arrays.sort(nums1);
+    }
+}
